ec4a321eb1ce6998a1ce9925a12c2a9bb11f60bd
Bala Chirtsabesan <balaTest@live.com>
2020-10-06 07:30:30 -0700

    Add project files.

---
+++ WebAppMvc_Anc31_core_NoAuth_1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppMvc_Anc31_core_NoAuth_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
